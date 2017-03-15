96f1a66c2d91538efc5b0089ca4c94416721c1a6
Ezequiel França <ezequiel.santos@btgpactual.com>
2017-03-15 15:37:57 -0300

    Update Tests

--- Example/FCBBarCodeGeneratorTests/FCBBarCodeGeneratorTests.swift
+++ Example/FCBBarCodeGeneratorTests/FCBBarCodeGeneratorTests.swift
@@ -20,13 +20,13 @@ class FCBBarCodeGeneratorTests: XCTestCase {
         XCTAssertNotNil(text)
         XCTAssertEqual(text, "testCodeBarGenerator")
         
-        let qrcode = codeGenerator.barcodeWithCode(text, type: .qrcode, size: size)
+        let qrcode = codeGenerator.barcode(code: text, type: .qrcode, size: size)
          XCTAssertNotNil(qrcode)
-        let pdf147 = codeGenerator.barcodeWithCode(text, type: .pdf417, size: size)
+        let pdf147 = codeGenerator.barcode(code: text, type: .pdf417, size: size)
          XCTAssertNotNil(pdf147)
-        let aztec = codeGenerator.barcodeWithCode(text, type: .aztec, size: size)
+        let aztec = codeGenerator.barcode(code: text, type: .aztec, size: size)
          XCTAssertNotNil(aztec)
-        let code128 = codeGenerator.barcodeWithCode(text, type: .code128, size: size)
+        let code128 = codeGenerator.barcode(code: text, type: .code128, size: size)
          XCTAssertNotNil(code128)
         
     }
